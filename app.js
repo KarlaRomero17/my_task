ab5d24ca10d62d8938a1a642bd74f95b0c5a17b1
Lissette Romero <romerolissette17@gmail.com>
2023-05-04 08:28:23 -0600

    Subiendo proyecto

---
+++ app.js
@@ -0,0 +1,16 @@
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
+require('colors');
+console.clear();
+
+const main = async() => {
+    console.log('Hola Mundo');
+    let opt = '';
+    do{
+        opt = await mostrarMenu();
+        console.log({ opt });
+        if( opt !== '0') await pausa();
+    }while( opt !== '0' ) ;
+}
+
+main();
